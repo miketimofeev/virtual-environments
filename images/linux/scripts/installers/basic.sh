416f2816c4517822e60821c598622bc82c9428ea
Mikhail Timofeev <v-mitim@microsoft.com>
2021-06-09 00:38:40 +0300

    full template

--- images/linux/scripts/installers/basic.sh
+++ images/linux/scripts/installers/basic.sh
@@ -4,6 +4,7 @@
 ##  Desc:  Installs basic command line utilities and dev packages
 ################################################################################
 source $HELPER_SCRIPTS/install.sh
+source $HELPER_SCRIPTS/os.sh
 
 common_packages=$(get_toolset_value .apt.common_packages[])
 cmd_packages=$(get_toolset_value .apt.cmd_packages[])
@@ -11,5 +12,15 @@ for package in $common_packages $cmd_packages; do
     echo "Install $package"
     apt-get install -y --no-install-recommends $package
 done
+
+if isUbuntu16; then
+    wget https://www.openssl.org/source/openssl-1.1.1k.tar.gz
+    tar xzvf openssl-1.1.1k.tar.gz
+    cd openssl-1.1.1k
+    ./config --openssldir=/etc/ssl '-Wl,--enable-new-dtags,-rpath,$(LIBRPATH)'
+    make
+    make install
+    ln -sf /etc/ssl/bin/openssl /usr/bin/openssl
+fi
 
 invoke_tests "Apt"
