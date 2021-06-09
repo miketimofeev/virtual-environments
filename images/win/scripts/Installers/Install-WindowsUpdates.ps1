5eb801d3747122ed58642da07436f743bc4f861f
Mikhail Timofeev <v-mitim@microsoft.com>
2021-06-09 18:25:16 +0300

    add a condition

--- images/win/scripts/Installers/Install-WindowsUpdates.ps1
+++ images/win/scripts/Installers/Install-WindowsUpdates.ps1
@@ -5,8 +5,9 @@
 ################################################################################
 
 Write-Host "Run windows updates"
-Get-WUInstall -MicrosoftUpdate
-Write-Host "Hide update KB5003638"
-Hide-WindowsUpdate -Confirm:$false -KBArticleID "KB5003638"
-Write-Host "Run windows updates"
+if (Test-IsWin16) {
+    Get-WUInstall -MicrosoftUpdate
+    Write-Host "Hide update KB5003638"
+    Hide-WindowsUpdate -Confirm:$false -KBArticleID "KB5003638"
+}
 Get-WUInstall -MicrosoftUpdate -AcceptAll -Install -IgnoreUserInput -IgnoreReboot
