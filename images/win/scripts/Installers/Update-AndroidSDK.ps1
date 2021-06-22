671b3503d119d6fd7b444543a22361fedfd3a88f
Mikhail Timofeev <v-mitim@microsoft.com>
2021-06-22 22:53:50 +0300

    Switch android sdkmanager to the command line one

--- images/win/scripts/Installers/Update-AndroidSDK.ps1
+++ images/win/scripts/Installers/Update-AndroidSDK.ps1
@@ -3,34 +3,27 @@
 ##  Desc:  Install and update Android SDK and tools
 ################################################################################
 
-# Install the standard Android SDK licenses. In the past, there wasn't a better way to do this,
-# so we are base64-encoding a zip of the licenses directory from another installation.
-# To create this base64 string, create a zip file that contains nothing but a 'licenses' folder,
-# which folder contains the accepted license files found in 'C:\Program Files (x86)\Android\android-sdk\licenses'.
-# Then, run this in PowerShell:
-#     $LicensesZipFileName = 'C:\Program Files (x86)\Android\android-sdk\Licenses.zip'
-#     $base64Content = [Convert]::ToBase64String([IO.File]::ReadAllBytes($LicensesZipFileName))
-#     echo $base64Content
-#
-# Future: see if the base64 technique can be avoided by running this PowerShell script to accept all licenses.
-# This fails when run on a live agent, likely because non-interactive mode is set.
-# It may work fine during image generation (this script).
-#     for($i=0; $i -lt 100; $i++) { $response += "y`n"}; $response | .\sdkmanager.bat --licenses
-$base64Content = "UEsDBBQAAAAAAKJeN06amkPzKgAAACoAAAAhAAAAbGljZW5zZXMvYW5kcm9pZC1nb29nbGV0di1saWNlbnNlDQpmYzk0NmU4ZjIzMWYzZTMxNTliZjBiN2M2NTVjOTI0Y2IyZTM4MzMwUEsDBBQAAAAIAKBrN05E+YSqQwAAAFQAAAAcAAAAbGljZW5zZXMvYW5kcm9pZC1zZGstbGljZW5zZQXByREAIQgEwP9WmYsjhxgOKJN/CNs9vmdOQ2zdRw2dxQnWjqQ/3oIgXQM9vqUiwkiX8ljWea4ZlCF3xTo1pz6w+wdQSwMEFAAAAAAAxV43TpECY7AqAAAAKgAAACQAAABsaWNlbnNlcy9hbmRyb2lkLXNkay1wcmV2aWV3LWxpY2Vuc2UNCjUwNDY2N2Y0YzBkZTdhZjFhMDZkZTlmNGIxNzI3Yjg0MzUxZjI5MTBQSwMEFAAAAAAAzF43TpOr0CgqAAAAKgAAABsAAABsaWNlbnNlcy9nb29nbGUtZ2RrLWxpY2Vuc2UNCjMzYjZhMmI2NDYwN2YxMWI3NTlmMzIwZWY5ZGZmNGFlNWM0N2Q5N2FQSwMEFAAAAAAAz143TqxN4xEqAAAAKgAAACQAAABsaWNlbnNlcy9pbnRlbC1hbmRyb2lkLWV4dHJhLWxpY2Vuc2UNCmQ5NzVmNzUxNjk4YTc3YjY2MmYxMjU0ZGRiZWVkMzkwMWU5NzZmNWFQSwMEFAAAAAAA0l43Tu2ee/8qAAAAKgAAACYAAABsaWNlbnNlcy9taXBzLWFuZHJvaWQtc3lzaW1hZ2UtbGljZW5zZQ0KNjNkNzAzZjU2OTJmZDg5MWQ1YWNhY2ZiZDhlMDlmNDBmYzk3NjEwNVBLAQIUABQAAAAAAKJeN06amkPzKgAAACoAAAAhAAAAAAAAAAEAIAAAAAAAAABsaWNlbnNlcy9hbmRyb2lkLWdvb2dsZXR2LWxpY2Vuc2VQSwECFAAUAAAACACgazdORPmEqkMAAABUAAAAHAAAAAAAAAABACAAAABpAAAAbGljZW5zZXMvYW5kcm9pZC1zZGstbGljZW5zZVBLAQIUABQAAAAAAMVeN06RAmOwKgAAACoAAAAkAAAAAAAAAAEAIAAAAOYAAABsaWNlbnNlcy9hbmRyb2lkLXNkay1wcmV2aWV3LWxpY2Vuc2VQSwECFAAUAAAAAADMXjdOk6vQKCoAAAAqAAAAGwAAAAAAAAABACAAAABSAQAAbGljZW5zZXMvZ29vZ2xlLWdkay1saWNlbnNlUEsBAhQAFAAAAAAAz143TqxN4xEqAAAAKgAAACQAAAAAAAAAAQAgAAAAtQEAAGxpY2Vuc2VzL2ludGVsLWFuZHJvaWQtZXh0cmEtbGljZW5zZVBLAQIUABQAAAAAANJeN07tnnv/KgAAACoAAAAmAAAAAAAAAAEAIAAAACECAABsaWNlbnNlcy9taXBzLWFuZHJvaWQtc3lzaW1hZ2UtbGljZW5zZVBLBQYAAAAABgAGANoBAACPAgAAAAA="
-$content = [System.Convert]::FromBase64String($base64Content)
-Set-Content -Path .\android-sdk-licenses.zip -Value $content -Encoding Byte
+$cmdlineToolsUrl = "https://dl.google.com/android/repository/commandlinetools-win-7302050_latest.zip"
+$cmdlineToolsArchPath = Start-DownloadWithRetry -Url $cmdlineToolsUrl -Name "cmdline-tools.zip"
 $sdkInstallRoot = "C:\Program Files (x86)\Android\android-sdk"
 $sdkRoot = "C:\Android\android-sdk"
-Expand-Archive -Path .\android-sdk-licenses.zip -DestinationPath $sdkInstallRoot -Force
+Expand-Archive -Path $cmdlineToolsArchPath -DestinationPath "${sdkInstallRoot}\cmdline-tools" -Force
+Rename-Item "${sdkInstallRoot}\cmdline-tools\cmdline-tools" "latest"
 New-Item -Path "C:\Android" -ItemType Directory
 New-Item -Path "$sdkRoot" -ItemType SymbolicLink -Value "$sdkInstallRoot"
-
-# run the updates.
-# keep newer versions in descending order
-
-# Get android content from toolset
 $androidToolset = (Get-ToolsetContent).android
-$sdkManager = "$sdkRoot\tools\bin\sdkmanager.bat"
+$sdkManager = "$sdkRoot\cmdline-tools\latest\bin\sdkmanager.bat"
+
+# Get licenses count, we need to add +1 to the output since the first sdkmanager request to review the licenses also requires Y answer
+(Write-Output N | & $sdkManager --licenses | Select-String -Pattern "SDK package licenses not accepted") -match "(?<count>^\d+) of \d+" | Out-Null
+$licensesCount = [int]$Matches.count + 1
+for($i=0; $i -lt $licensesCount; $i++)
+{
+    $response += "y`n"
+}
+
+# Accept all the licenses
+$response | & $sdkManager --licenses | Out-Null
 
 & $sdkManager --sdk_root=$sdkRoot "platform-tools"
 
