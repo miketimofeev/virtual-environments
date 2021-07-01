b7c72344578283e3f75c8e5ecc084f5351b3f3cd
Mikhail Timofeev <v-mitim@microsoft.com>
2021-07-01 09:31:53 +0300

    Add back commandline tools to report

--- images/linux/scripts/SoftwareReport/SoftwareReport.Android.psm1
+++ images/linux/scripts/SoftwareReport/SoftwareReport.Android.psm1
@@ -23,11 +23,10 @@ function Get-AndroidInstalledPackages {
 
 function Build-AndroidTable {
     $packageInfo = Get-AndroidInstalledPackages
-    $commandLineTools = Get-AndroidSDKManagerPath
     return @(
         @{
             "Package" = "Android Command Line Tools"
-            "Version" = (& $commandLineTools --version 2>$null | Out-String).Trim()
+            "Version" = Get-AndroidCommandLineToolsVersion
         },
         @{
             "Package" = "Android Emulator"
@@ -114,6 +113,13 @@ function Get-AndroidPlatformVersions {
     }
     [array]::Reverse($versions)
     return ($versions -Join "<br>")
+}
+
+function Get-AndroidCommandLineToolsVersion {
+    $commandLineTools = Get-AndroidSDKManagerPath
+    (& $commandLineTools --version | Out-String).Trim() -match "^\d+\.{1,}\d+$" | Out-Null
+    $commandLineToolsVersion = $Matches.Values
+    return $commandLineToolsVersion
 }
 
 function Get-AndroidBuildToolVersions {
